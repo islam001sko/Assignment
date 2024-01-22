dbf49fc30ff56eb3d8c4939885f896885d1796f7
islam001sko <zhaslanuly01@gmail.com>
2024-01-22 23:28:38 +0600

    a

--- routes/app.js
+++ routes/app.js
@@ -1,15 +1,12 @@
-// server/app.js
 const express = require('express');
 const path = require('path');
 const app = express();
 const weatherRouter = require('./weather');
 
-// Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/weather', weatherRouter);
 
-// Update the route handler for the root path
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
